b1946c1293aa488494d389dc12a7bf05aa89be14
陈宇峰 <yufengcode@gmail.com>
2016-10-05 22:55:25 +0800

    Initial Commit

---
+++ 5. Longest Palindromic Substring/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  5. Longest Palindromic Substring
+//
+//  Created by chenyufeng on 10/5/16.
+//  Copyright © 2016 chenyufeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
